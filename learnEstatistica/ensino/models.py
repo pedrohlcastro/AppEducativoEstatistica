0dc9267a74a8bff35363f42dfbbfa0bfe0199076
pedrohlcastro <pedrohlcastro@gmail.com>
2016-09-16 10:15:10 -0300

    Ensino Pronto

--- learnEstatistica/ensino/models.py
+++ learnEstatistica/ensino/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # Create your models here.
 class Ensino(models.Model):
     name = models.CharField(max_length = 200)
-    number = models.PositiveIntegerField(default = 1)
+    number = models.CharField(max_length = 3,default = 1)
     title = models.CharField(max_length = 400, default='titulo')
     fulltext = models.TextField()
-    link = models.URLField(max_length = 200)
+    link = models.CharField('Caso -1 vai para questoes',max_length = 3, default = 2)
     def __str__(self):
         return str(self.name)
