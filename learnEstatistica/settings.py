1434db54d8941ae4711d3f15ed824c35785a66a1
Pedro Henrique Lopes de Castro <pedrohlcastro@gmail.com>
2017-01-16 20:44:42 -0200

    tentantiva 3 deploy

--- learnEstatistica/settings.py
+++ learnEstatistica/settings.py
@@ -139,3 +139,17 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
