2b7840d007fb73b949d19198b3d54efa2b2e15b5
Pedro Henrique Lopes de Castro <pedrohlcastro@gmail.com>
2017-01-16 20:19:43 -0200

    Tentativa de Deploy

--- learnEstatistica/learnEstatistica/settings.py
+++ learnEstatistica/learnEstatistica/settings.py
@@ -131,3 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticRoot/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
